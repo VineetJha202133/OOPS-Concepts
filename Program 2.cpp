4b96e422342575c281976250eba57a0e39495a69
Vineet Jha <89996361+VineetJha202133@users.noreply.github.com>
2022-04-10 23:50:05 +0530

    Create Program 2.cpp

---
+++ Program 2.cpp
@@ -0,0 +1,18 @@
+//Program to make the use of inline functions.
+#include <iostream>
+
+using namespace std;
+
+inline int add(int a, int b)
+{
+    return a+b;
+}
+
+int main()
+{
+    int x, y, res;
+    cout << "Enter the numbers: ";
+    cin >> x >> y;
+    res = add(x,y);
+    cout << "Summation of two numbers is " << res;
+}
